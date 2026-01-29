d646bc03c144a183f015fa1e51ee3dfcd94f2867
Julius Rayray <jlsrayray@gmail.com>
2026-01-30 01:16:27 +0800

    Update Pic

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -48,7 +48,7 @@ export function About() {
 
             <div className="relative aspect-square rounded-lg overflow-hidden border border-primary/20">
               <ImageWithFallback
-                src="https://images.unsplash.com/photo-1614179924047-e1ab49a0a0cf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxnYW1pbmclMjBzZXR1cHxlbnwxfHx8fDE3NjM3MzE4MDZ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
+                src="../Assets/Formal photo.jpg"
                 alt="Gaming setup"
                 className="w-full h-full object-cover"
               />
